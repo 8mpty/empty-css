5ea367e662387bad0eaee5b2e20da1a5169a53fa
8mpty <mohdkhairin62@gmail.com>
2023-08-19 05:44:41 +0800

    added user.js

---
+++ user.js
@@ -0,0 +1,40 @@
+user_pref("", false);
+
+// MISC
+user_pref("app.update.service.enabled", false);
+user_pref("browser.download.autohideButton", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeBookmarks", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeDownloads", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includePocket", false);
+user_pref("browser.newtabpage.activity-stream.section.highlights.includeVisited", false);
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.preferences.defaultPerformanceSettings.enabled", false);
+user_pref("app.update.service.enabled", false);
+user_pref("browser.download.panel.shown", true);
+user_pref("layers.acceleration.disabled", true);
+user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+user_pref("app.update.mode", 1);
+user_pref("browser.dataFeatureRecommendations.enabled", false);
+user_pref("browser.startup.page", 0);
+user_pref("browser.startup.homepage", "about:blank");
+user_pref("browser.newtabpage.enabled", false);
+user_pref("browser.newtab.url", "about:blank");
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("app.shield.optoutstudies.enabled", false);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.search.defaultenginename", "DuckDuckGo");
+user_pref("browser.search.selectedEngine", "DuckDuckGo");
+user_pref("browser.search.searchEnginesURL", "https://duckduckgo.com/");
+user_pref("keyword.URL", "https://duckduckgo.com/?q=");
+
+
+// Dev Tools
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.remote-enabled", true);
+
+
+// Firefox CSS
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
